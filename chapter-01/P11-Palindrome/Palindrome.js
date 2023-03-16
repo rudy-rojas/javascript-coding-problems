50510743439251b87fa7b34ae1c24e1dc2147332
rudy-rojas <rudy.rs1@gmail.com>
2023-03-16 16:35:17 -0400

    (Ch01-P11) Palindrome A second solution is added.

--- chapter-01/P11-Palindrome/Palindrome.js
+++ chapter-01/P11-Palindrome/Palindrome.js
@@ -1,17 +1,22 @@
-const isPalindrome = (str) => {
-  let limit = parseInt(str.length / 2);
-  let i = 0;
-  let j = str.length - 1;
-  let result = true;
-  while (i < limit && result) {
-    if (str.charAt(i) != str.charAt(j)) {
-      result = false;
-    } else {
-      i++;
-      j--;
-    }
-  }
-  return result;
-};
+// Method 02
+const isPalindrome = (str) =>
+  str === str.toLowerCase().split('').reverse().join('');
+
+// Method 01
+// const isPalindrome = (str) => {
+//   let limit = parseInt(str.length / 2);
+//   let i = 0;
+//   let j = str.length - 1;
+//   let result = true;
+//   while (i < limit && result) {
+//     if (str.charAt(i) != str.charAt(j)) {
+//       result = false;
+//     } else {
+//       i++;
+//       j--;
+//     }
+//   }
+//   return result;
+// };
 
 module.exports = isPalindrome;
