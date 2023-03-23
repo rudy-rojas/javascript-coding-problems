10e13f156689b2ec7a1a7a472cd030330543e0e8
rudy-rojas <rudy.rs1@gmail.com>
2023-03-23 19:00:58 -0400

    (Ch01-P22) FindLongestCommonPrefix, algorithm done.

---
+++ chapter-01/P22-FindLongestCommonPrefix/FindLongestCommonPrefix.js
@@ -0,0 +1,25 @@
+const findPrefix = (array) => {
+  let prefix = '';
+  let isEqual = true;
+  let i = 0;
+  let minWord = array.reduce((acc, el) => (acc.length <= el.length ? acc : el));
+  let j = 0;
+  while (i < minWord.length && isEqual) {
+    j = 0;
+    while (j < array.length && isEqual) {
+      if (minWord.charAt(i) !== array[j].charAt(i)) {
+        isEqual = false;
+      }
+      j++;
+    }
+    if (isEqual) {
+      prefix += minWord.charAt(i);
+    }
+    i++;
+  }
+
+  return prefix;
+};
+
+ const input = ['ab', 'abc', 'abcd', 'abcde'];
+console.log(findPrefix(input));
---
+++ chapter-01/P22-FindLongestCommonPrefix/README.md
@@ -0,0 +1,30 @@
+## Chapter 01 - Problem 22. Find longest common prefix
+
+## Description:
+
+Write a program that finds the longest common prefix of given strings.
+
+<table>
+  <tr>
+    <th> Input </th> <th> Result </th>
+  </tr>
+  <tr>
+    <td>
+
+```javascript
+const input = ['apple', 'appetite', 'application', 'appendix'];
+```
+
+  </td>
+<td>
+
+```javascript
+'app'
+```
+
+  </td>
+  </tr>
+
+</table>
+
+_<strong>Credits:</strong> Exercise taken from the book <strong>'Java Coding Problems'</strong> by author <strong>'Anghel Leonard' (Packt)</strong>, Chapter 01, Problem 22_
