58fbf8391950ddc765762b55df6dfca5552ab34c
rudy-rojas <rudy.rs1@gmail.com>
2023-03-24 17:44:30 -0400

    (Ch01-24) Transform Strings, done.

---
+++ chapter-01/P24-TransformStrings/README.md
@@ -0,0 +1,9 @@
+
+## Chapter 01 -  Problem 24. Transform strings
+
+## Description:
+
+Write several snippets of code for transforming a string in another string.
+
+
+_<strong>Credits:</strong> Exercise taken from the book <strong>'Java Coding Problems'</strong> by author <strong>'Anghel Leonard' (Packt)</strong>, Chapter 01, Problem 24_
---
+++ chapter-01/P24-TransformStrings/TransformStrings.js
@@ -0,0 +1,14 @@
+
+const originalString1 = "Hello World!";
+const newString1 = originalString1.replace("World", "Universe");
+console.log(newString1);
+
+
+const originalString2 = "Today is a sunny day!";
+const newString2 = originalString2.toUpperCase();
+console.log(newString2);
+
+
+const originalString3 = "The quick brown fox jumps over the lazy dog";
+const newString3 = originalString3.split(" ").reverse().join(" ");
+console.log(newString3);
